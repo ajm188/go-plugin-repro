25b9c6b655e35d8132bfb016301cb67a31a3c0d1
Andrew Mason <amason@slack-corp.com>
2020-11-22 20:30:00 -0500

    This, however, works

--- lib/lib.go
+++ lib/lib.go
@@ -20,10 +20,10 @@ func Load(path string) (Fn, error) {
 		return nil, err
 	}
 
-	fn, ok := f.(Fn)
+	fn, ok := f.(*Fn)
 	if !ok {
 		return nil, fmt.Errorf("wrong type for %+v", f)
 	}
 
-	return fn, nil
+	return *fn, nil
 }
